4a6f207db90ea8b0c6f9413cc922eb7c91a3b4c2
vikas <vikas0352004@gmail.com>
2026-09-22 10:59:47 +0530

    remove env and node_modules from repository

--- server/controllers/course.controllers.js
+++ server/controllers/course.controllers.js
@@ -1,5 +1,7 @@
-import TryCatch from "../middlewares/tryCatch";
-import { Course } from "../models/course_model";
+import TryCatch from "../middlewares/tryCatch.js";
+import { Course } from "../models/course_model.js";
+import { Lecutre } from "../models/lecture.js";
+import { User } from "../models/user_model.js";
 
 export const gelAllCoures=TryCatch(async(req,res)=>{
 
@@ -18,6 +20,58 @@ res.json({
 export const getSingleCourse=TryCatch(async(req,res)=>{
 
 const course=await Course.findById(req.params.id)
+ 
+res.json({
+    course
+})
+
+
+})
+
+
+export const fetchLectures=TryCatch(async(req,res)=>{
+    const lectures=await Lecutre.find({course:req.params.id})
+
+    const user=await User.findById(req.user._id)
+
+     if(user.role==="admin"){
+        return res.json({
+            lectures
+        })
+     }
+
+
+     if(!user.subscription.includes(req.params.id)){
+        return res.status(400).json({
+            message:"You have not subscribed to this course"
+        })
+     }
+ return res.json({lectures})
+
+
+})
+
+
+
+
+export const fetchLecture=TryCatch(async(req,res)=>{
+    const lecture=await Lecutre.findById(req.params.id)
+
+    const user=await User.findById(req.user._id)
+
+     if(user.role==="admin"){
+        return res.json({
+            lectures
+        })
+     }
+
+
+     if(!user.subscription.includes(req.params.id)){
+        return res.status(400).json({
+            message:"You have not subscribed to this course"
+        })
+     }
+ return res.json({lecture})
 
 
 })
--- server/routes/course_routes.js
+++ server/routes/course_routes.js
@@ -1,6 +1,9 @@
 import express from "express"
+import { gelAllCoures, getSingleCourse } from "../controllers/course.controllers.js"
 
 const router=express.Router()
 
+router.get("/course/all",gelAllCoures)
+router.get("/course/:id",getSingleCourse)
 
 export default router
